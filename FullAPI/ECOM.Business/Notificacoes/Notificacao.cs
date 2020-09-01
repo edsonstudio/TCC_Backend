51379c1146403c0ed3fac29057e4f84204b890bd
Edson <edsont8@hotmail.com>
2020-08-31 22:11:36 -0300

    Primeiras notificacoes

---
+++ FullAPI/ECOM.Business/Notificacoes/Notificacao.cs
@@ -0,0 +1,12 @@
+﻿namespace ECOM.Business.Notificacoes
+{
+    public class Notificacao
+    {
+        public string Mensagem { get; }
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+
+    }
+}
