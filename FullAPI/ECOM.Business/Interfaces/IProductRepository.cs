8834d9a8456195b5a3f475cf97e923251cb6df6b
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-09-01 19:55:04 -0300

    Interface generica de repositorio + abstração produto repositorio

---
+++ FullAPI/ECOM.Business/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ECOM.API.Models;
+
+namespace ECOM.Business.Interfaces
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<IEnumerable<Product>> ObterProdutosPorFornecedor(Guid fornecedorId);
+        Task<IEnumerable<Product>> ObterProdutosFornecedores();
+        Task<Product> ObterProdutoFornecedor(Guid id);
+    }
+}
