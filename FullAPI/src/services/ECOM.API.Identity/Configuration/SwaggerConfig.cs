f80cb08a7d0a81651afd47c5c70d161aa5ed2105
Edson <edsont8@hotmail.com>
2020-09-17 10:05:28 -0300

    Refatoracao e melhorias

---
+++ FullAPI/src/services/ECOM.API.Identity/Configuration/SwaggerConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace ECOM.API.Identity.Configuration
+
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ECOM - Ecommerce Identity API",
+                    Description = "Esta API faz parte do projeto de TCC do Curso de Desenvolvimento de Sistemas - SENAI-SP",
+                    Contact = new OpenApiContact() { Name = "Edson Costa", Email = "edsont8@hotmail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
+
+
