45f0a418777b7f7d3ed8cbc1e92bd1e91995bbd0
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-09-18 19:52:09 -0300

    Centralizando model Entity no projeto core

--- FullAPI/ECOM.Business/Models/AssociatedProducts.cs
+++ FullAPI/ECOM.Business/Models/AssociatedProducts.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ECOM.Core.DomainObjects;
 
 namespace ECOM.Business.Models
 {
--- FullAPI/ECOM.Business/Models/Product.cs
+++ FullAPI/ECOM.Business/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ECOM.Core.DomainObjects;
 
 namespace ECOM.Business.Models
 {
