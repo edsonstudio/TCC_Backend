ca1f31f52a95591071f4e617ab0ddb1bc66f37e7
Edson <edsont8@hotmail.com>
2020-09-19 21:20:07 -0300

    Correção heranca Entity

---
+++ FullAPI/ECOM.Business/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECOM.Business.Models
+{
+    public abstract class Entity
+    {
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Column(TypeName = "varchar(36)")]
+        public Guid Id { get; set; }
+    }
+}
