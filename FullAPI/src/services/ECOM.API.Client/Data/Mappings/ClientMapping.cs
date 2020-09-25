92dc0c110202d8ec35495643e88bc8e02b408849
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-09-25 19:49:20 -0300

    Erro da Secret Key resolvido, Bug com Construtor de endereco

--- FullAPI/src/services/ECOM.API.Client/Data/Mappings/ClientMapping.cs
+++ FullAPI/src/services/ECOM.API.Client/Data/Mappings/ClientMapping.cs
@@ -2,6 +2,7 @@
 using System;
 using ECOM.API.Client.Models;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ECOM.Core.DomainObjects;
 
 namespace ECOM.API.Client.Data.Mappings
 {
@@ -11,23 +12,23 @@ namespace ECOM.API.Client.Data.Mappings
         {
             builder.HasKey(c => c.Id);
 
-            //builder.Property(c => c.Name).IsRequired().HasColumnType("varchar(200)");
+            builder.Property(c => c.Name).IsRequired().HasColumnType("varchar(200)");
 
-            /*builder.OwnsOne(c => c.CPF, tf =>
+            builder.OwnsOne(c => c.CPF, tf =>
             {
                 tf.Property(c => c.Numero)
                 .IsRequired()
                 .HasMaxLength(Cpf.CpfMaxLength)
                 .HasColumnName("Cpf")
                 .HasColumnType($"varchar({Cpf.CpfMaxLength})");
-            });*/
+            });
 
-            //builder.Property(c => c.Phone).IsRequired().HasMaxLength(9).HasColumnType("varchar(9)");
+            builder.Property(c => c.Phone).IsRequired().HasMaxLength(9).HasColumnType("varchar(9)");
 
             // 1 : N => Cliente : Endereco
             builder.HasMany(c => c.Address).WithOne(c => c.Client);
 
-            //builder.ToTable("Clients");
+            builder.ToTable("Clients");
         }
     }
 }
