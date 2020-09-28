8550879f66eabaca24d96d84e0771541f8362461
Edson <edsont8@hotmail.com>
2020-09-28 10:18:07 -0300

    Estrutura de dados da API

---
+++ FullAPI/src/services/ECOM.API.Carrinho/Model/CarrinhoCliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECOM.API.Carrinho.Model
+{
+    public class CarrinhoCliente
+    {
+        public Guid Id { get; set; }
+        public Guid ClientId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<CarrinhoItem> Items { get; set; } = new List<CarrinhoItem>();
+
+        public CarrinhoCliente(Guid clientId)
+        {
+            Id = Guid.NewGuid();
+            ClientId = clientId;
+        }
+
+        public CarrinhoCliente(){}
+    }
+}
