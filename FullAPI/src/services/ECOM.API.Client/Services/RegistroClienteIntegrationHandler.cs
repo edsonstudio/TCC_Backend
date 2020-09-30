ba0625f17ba27aed9b9788c39bd79f296742513e
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-09-30 17:43:21 -0300

    Customizando Event Bus 3

--- FullAPI/src/services/ECOM.API.Client/Services/RegistroClienteIntegrationHandler.cs
+++ FullAPI/src/services/ECOM.API.Client/Services/RegistroClienteIntegrationHandler.cs
@@ -1,7 +1,7 @@
-﻿using EasyNetQ;
-using ECOM.API.Client.Application.Commands;
+﻿using ECOM.API.Client.Application.Commands;
 using ECOM.Core.Mediator;
 using ECOM.Core.Messages.Integration;
+using ECOM.MessageBus;
 using FluentValidation.Results;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,24 +13,25 @@ namespace ECOM.API.Client.Services
 {
     public class RegistroClienteIntegrationHandler : BackgroundService
     {
-        private IBus _bus;
+        private readonly IMessageBus _bus;
         private readonly IServiceProvider _serviceProvider;
-        public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider)
+        public RegistroClienteIntegrationHandler(IServiceProvider serviceProvider,
+                                                 IMessageBus bus)
         {
             _serviceProvider = serviceProvider;
+            _bus = bus;
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _bus = RabbitHutch.CreateBus("host:localhost:5672");
 
             _bus.RespondAsync<UsuarioRegistradoIntegrationEvent, ResponseMessage>(async request =>
-                new ResponseMessage(await RegistrarCliente(request)));
+                await RegistrarCliente(request));
 
             return Task.CompletedTask;
         }
 
-        private async Task<ValidationResult> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
+        private async Task<ResponseMessage> RegistrarCliente(UsuarioRegistradoIntegrationEvent message)
         {
             var clienteCommand = new RegisterClientCommand(message.Id, message.Name, message.Email, message.Cpf, message.Phone);
             ValidationResult sucesso;
@@ -41,7 +42,7 @@ namespace ECOM.API.Client.Services
                 sucesso = await mediator.EnviarComando(clienteCommand);
             }
 
-            return sucesso;
+            return new ResponseMessage(sucesso);
         }
     }
 }
