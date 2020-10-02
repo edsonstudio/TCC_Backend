592e77414ac01fce97190ac5b8762cf23149c3d4
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-10-02 15:57:55 -0300

    Mapeamento das entidades de pagamento

---
+++ FullAPI/src/services/ECOM.API.Payment/Data/Mappings/PagamentoMapping.cs
@@ -0,0 +1,23 @@
+﻿using ECOM.API.Payment.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECOM.API.Payment.Data.Mappings
+{
+    public class PagamentoMapping : IEntityTypeConfiguration<Pagamento>
+    {
+        public void Configure(EntityTypeBuilder<Pagamento> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Ignore(c => c.CartaoCredito);
+
+            // 1 : N => Pagamento : Transacao
+            builder.HasMany(c => c.Transacoes)
+                .WithOne(c => c.Pagamento)
+                .HasForeignKey(c => c.PagamentoId);
+
+            //builder.ToTable("Pagamentos");
+        }
+    }
+}
