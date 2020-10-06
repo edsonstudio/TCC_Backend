2d9d2e74364377dcc35ba05d957d4984d044e2e3
Edson <edsont8@hotmail.com>
2020-10-06 18:26:04 -0300

    Application Query Stack e Specification Pattern

---
+++ FullAPI/src/services/ECOM.Pedidos.Domain/Vouchers/Specs/VoucherSpec.cs
@@ -0,0 +1,30 @@
+﻿using NetDevPack.Specification;
+using System;
+using System.Linq.Expressions;
+
+namespace ECOM.Pedidos.Domain.Vouchers.Specs
+{
+    public class VoucherDateSpecification : Specification<Voucher>
+    {
+        public override Expression<Func<Voucher, bool>> ToExpression()
+        {
+            return voucher => voucher.DataValidade >= DateTime.Now;
+        }
+    }
+
+    public class VoucherQuantidadeSpecification : Specification<Voucher>
+    {
+        public override Expression<Func<Voucher, bool>> ToExpression()
+        {
+            return voucher => voucher.Quantidade > 0;
+        }
+    }
+
+    public class VoucherAtivoSpecification : Specification<Voucher>
+    {
+        public override Expression<Func<Voucher, bool>> ToExpression()
+        {
+            return voucher => voucher.Ativo && !voucher.Utilizado;
+        }
+    }
+}
