585f8f8f5ef4223fd6e95e97fff8f01874cb7b04
Edson <edsont8@hotmail.com>
2020-10-05 21:50:28 -0300

    Consumindo serviços pelo BFF - pt II

--- FullAPI/src/api gateways/ECOM.Bff.Compras/Services/CatalogoService.cs
+++ FullAPI/src/api gateways/ECOM.Bff.Compras/Services/CatalogoService.cs
@@ -1,12 +1,15 @@
 ﻿using ECOM.Bff.Compras.Extensions;
+using ECOM.Bff.Compras.Models;
 using Microsoft.Extensions.Options;
 using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ECOM.Bff.Compras.Services
 {
     public interface ICatalogoService
     {
+        Task<ItemProdutoDTO> ObterPorId(Guid id);
     }
 
     public class CatalogoService : Service, ICatalogoService
@@ -17,6 +20,15 @@ namespace ECOM.Bff.Compras.Services
         {
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
+        }
+
+        public async Task<ItemProdutoDTO> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/Products/{id}"); //Verificar se necessita do caminho completo: /api/v1/...
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ItemProdutoDTO>(response);
         }
     }
 }
--- FullAPI/src/api gateways/ECOM.Bff.Compras/V1/Controllers/CarrinhoController.cs
+++ FullAPI/src/api gateways/ECOM.Bff.Compras/V1/Controllers/CarrinhoController.cs
@@ -1,7 +1,10 @@
-﻿using ECOM.Bff.Compras.Services;
+﻿using ECOM.Bff.Compras.Models;
+using ECOM.Bff.Compras.Services;
 using ECOM.WebAPI.Core.Controllers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ECOM.Bff.Compras.V1.Controllers
@@ -24,35 +27,82 @@ namespace ECOM.Bff.Compras.V1.Controllers
         [Route("compras/carrinho")]
         public async Task<IActionResult> Index()
         {
-            return CustomResponse();
+            return CustomResponse(await _carrinhoService.ObterCarrinho());
         }
 
         [HttpGet]
         [Route("compras/carrinho-quantidade")]
-        public async Task<IActionResult> ObterQuantidadeCarrinho()
+        public async Task<int> ObterQuantidadeCarrinho()
         {
-            return CustomResponse();
+            var quantidade = await _carrinhoService.ObterCarrinho();
+            return quantidade?.Itens.Sum(i => i.Quantidade) ?? 0;
         }
 
         [HttpPost]
         [Route("compras/carrinho/items")]
-        public async Task<IActionResult> AdicionarItemCarrinho()
+        public async Task<IActionResult> AdicionarItemCarrinho(ItemCarrinhoDTO itemProduto)
         {
-            return CustomResponse();
+            var produto = await _catalogoService.ObterPorId(itemProduto.ProdutoId);
+
+            await ValidarItemCarrinho(produto, itemProduto.Quantidade);
+            if (!OperacaoValida()) return CustomResponse();
+
+            itemProduto.Nome = produto.Nome;
+            itemProduto.Valor = produto.Valor;
+            itemProduto.Imagem = produto.Imagem;
+
+            var resposta = await _carrinhoService.AdicionarItemCarrinho(itemProduto);
+
+            return CustomResponse(resposta);
         }
 
         [HttpPut]
         [Route("compras/carrinho/items/{productId}")]
-        public async Task<IActionResult> AtualizarItemCarrinho()
+        public async Task<IActionResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoDTO itemProduto)
         {
-            return CustomResponse();
+            var produto = await _catalogoService.ObterPorId(produtoId);
+
+            await ValidarItemCarrinho(produto, itemProduto.Quantidade);
+            if (!OperacaoValida()) return CustomResponse();
+
+            var resposta = await _carrinhoService.AtualizarItemCarrinho(produtoId, itemProduto);
+            
+            return CustomResponse(resposta);
         }
 
         [HttpDelete]
         [Route("compras/carrinho/items/{productId}")]
-        public async Task<IActionResult> RemoverItemCarrinho()
+        public async Task<IActionResult> RemoverItemCarrinho(Guid produtoId)
         {
-            return CustomResponse();
+            var produto = await _catalogoService.ObterPorId(produtoId);
+
+            if (produto == null)
+            {
+                AdicionarErroProcessamento("Produto inexistente!");
+                return CustomResponse();
+            }
+
+            var resposta = await _carrinhoService.RemoverItemCarrinho(produtoId);
+
+            return CustomResponse(resposta);
+        }
+
+        private async Task ValidarItemCarrinho(ItemProdutoDTO produto, int quantidade)
+        {
+            if (produto == null) AdicionarErroProcessamento("Produto inexistente!");
+            if(quantidade < 1) AdicionarErroProcessamento($"Escolha ao menos uma unidade do produto {produto.Nome}");
+
+            var carrinho = await _carrinhoService.ObterCarrinho();
+            var itemCarrinho = carrinho.Itens.FirstOrDefault(p => p.ProdutoId == produto.Id);
+
+            if (itemCarrinho != null && itemCarrinho.Quantidade + quantidade > produto.QuantidadeEstoque)
+            {
+                AdicionarErroProcessamento
+                    ($"O produto {produto.Nome} possui {produto.QuantidadeEstoque} unidades em estoque, você selecionou {quantidade}");
+            }
+
+            if (quantidade > produto.QuantidadeEstoque) AdicionarErroProcessamento
+                    ($"O produto {produto.Nome} possui {produto.QuantidadeEstoque} unidades em estoque, você selecionou {quantidade}");
         }
     }
 }
