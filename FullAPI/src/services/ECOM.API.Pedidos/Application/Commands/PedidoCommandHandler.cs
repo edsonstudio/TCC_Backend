06e8b07c8f7b261153c3e20acaa83913f19ba886
Edson <edsont8@hotmail.com>
2020-10-07 22:01:17 -0300

    Camada de application de pedido - pt I

---
+++ FullAPI/src/services/ECOM.API.Pedidos/Application/Commands/PedidoCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using ECOM.Core.Messages;
+using FluentValidation.Results;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ECOM.API.Pedidos.Application.Commands
+{
+    public class PedidoCommandHandler : CommandHandler,
+        IRequestHandler<AdicionarPedidoCommand, ValidationResult>
+    {
+        public Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
+        {
+            // Validação do comando
+
+            // Mapear Pedido
+
+            // Aplicar voucher se houver
+
+            // Validar pedido
+
+            // Processar pagamento
+
+            // Se pagamento tudo ok!
+
+            // Adicionar Evento
+
+            // Adicionar Pedido Repositorio
+
+            // Persistir dados de pedido e voucher
+        }
+    }
+}
---
+++ FullAPI/src/services/ECOM.API.Pedidos/Application/DTO/PedidoItemDTO.cs
@@ -0,0 +1,21 @@
+﻿using ECOM.Pedidos.Domain.Pedidos;
+using System;
+
+namespace ECOM.API.Pedidos.Application.DTO
+{
+    public class PedidoItemDTO
+    {
+        public Guid PedidoId { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Image { get; set; }
+        public int Amount { get; set; }
+
+        public static PedidoItem ParaPedidoItem(PedidoItemDTO pedidoItemDTO)
+        {
+            return new PedidoItem(pedidoItemDTO.ProductId, pedidoItemDTO.Name, pedidoItemDTO.Amount,
+                pedidoItemDTO.Price, pedidoItemDTO.Image);
+        }
+    }
+}
