daf3310cca15b607ffeb1e89cce06c34b00556dd
Edson <edsont8@hotmail.com>
2020-10-08 06:29:15 -0300

    Finalizando a API de Pedido

--- FullAPI/src/services/ECOM.API.Pedidos/Configuration/DependencyInjectionConfig.cs
+++ FullAPI/src/services/ECOM.API.Pedidos/Configuration/DependencyInjectionConfig.cs
@@ -1,9 +1,14 @@
-﻿using ECOM.API.Pedidos.Application.Queries;
+﻿using ECOM.API.Pedidos.Application.Commands;
+using ECOM.API.Pedidos.Application.Events;
+using ECOM.API.Pedidos.Application.Queries;
 using ECOM.Core.Mediator;
+using ECOM.Pedidos.Domain.Pedidos;
 using ECOM.Pedidos.Domain.Vouchers;
 using ECOM.Pedidos.Infra.Data;
 using ECOM.Pedidos.Infra.Data.Repository;
 using ECOM.WebAPI.Core.Usuario;
+using FluentValidation.Results;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -19,12 +24,20 @@ namespace ECOM.API.Pedidos.Configuration
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
+            // Commands
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+
+            // Events
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
+
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
 
             // Data
             services.AddScoped<IVoucherRepository, VoucherRepository>();
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
             services.AddScoped<PedidosContext>();
 
             services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
